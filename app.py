c509389eafda4bd9b95527d76b823fc94e31a8b1
adi2710 <2710adityasingh@gmail.com>
2022-05-27 15:51:16 +0530

    Uploaded my project

---
+++ app.py
@@ -0,0 +1,124 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from keras.applications.vgg19 import VGG19, preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'best_model.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+model.make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+ref = {0: 'Apple___Apple_scab',
+ 1: 'Apple___Black_rot',
+ 2: 'Apple___Cedar_apple_rust',
+ 3: 'Apple___healthy',
+ 4: 'Blueberry___healthy',
+ 5: 'Cherry_(including_sour)___Powdery_mildew',
+ 6: 'Cherry_(including_sour)___healthy',
+ 7: 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+ 8: 'Corn_(maize)___Common_rust_',
+ 9: 'Corn_(maize)___Northern_Leaf_Blight',
+ 10: 'Corn_(maize)___healthy',
+ 11: 'Grape___Black_rot',
+ 12: 'Grape___Esca_(Black_Measles)',
+ 13: 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+ 14: 'Grape___healthy',
+ 15: 'Orange___Haunglongbing_(Citrus_greening)',
+ 16: 'Peach___Bacterial_spot',
+ 17: 'Peach___healthy',
+ 18: 'Pepper,_bell___Bacterial_spot',
+ 19: 'Pepper,_bell___healthy',
+ 20: 'Potato___Early_blight',
+ 21: 'Potato___Late_blight',
+ 22: 'Potato___healthy',
+ 23: 'Raspberry___healthy',
+ 24: 'Soybean___healthy',
+ 25: 'Squash___Powdery_mildew',
+ 26: 'Strawberry___Leaf_scorch',
+ 27: 'Strawberry___healthy',
+ 28: 'Tomato___Bacterial_spot',
+ 29: 'Tomato___Early_blight',
+ 30: 'Tomato___Late_blight',
+ 31: 'Tomato___Leaf_Mold',
+ 32: 'Tomato___Septoria_leaf_spot',
+ 33: 'Tomato___Spider_mites Two-spotted_spider_mite',
+ 34: 'Tomato___Target_Spot',
+ 35: 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 36: 'Tomato___Tomato_mosaic_virus',
+ 37: 'Tomato___healthy'}
+
+def model_predict(img_path, model):
+    #Loading the image
+    img = load_img(img_path, target_size = (256, 256))
+
+    #Preprocessing the image
+    i = img_to_array(img)
+
+    im = preprocess_input(i)
+
+    img = np.expand_dims(im, axis = 0)
+
+    #Making the prediction
+    pred = np.argmax(model.predict(img))
+    return ref[pred]
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        result = preds.replace("_", " ")                # Convert to string
+        # result = re.sub('+', ' ', result)
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
